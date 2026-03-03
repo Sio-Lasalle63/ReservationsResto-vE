7f7390358df5a52cfd5824e89293e53920efd555
Anthony <amedassi@lasalle63.fr>
2026-03-03 11:03:12 +0100

    Update MainViewController.java

--- src/main/java/com/medassi/reservationsrestaurant/controller/MainViewController.java
+++ src/main/java/com/medassi/reservationsrestaurant/controller/MainViewController.java
@@ -1,5 +1,6 @@
 package com.medassi.reservationsrestaurant.controller;
 
+import com.medassi.reservationsrestaurant.model.Forme;
 import com.medassi.reservationsrestaurant.model.Reservation;
 import com.medassi.reservationsrestaurant.model.Service;
 import com.medassi.reservationsrestaurant.model.Salle;
@@ -16,7 +17,12 @@ import javafx.scene.text.Text;
 import java.util.List;
 import java.util.ResourceBundle;
 import javafx.collections.FXCollections;
+import javafx.event.EventHandler;
 import javafx.fxml.Initializable;
+import javafx.scene.paint.Paint;
+import javafx.scene.shape.Circle;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.shape.Shape;
 
 /**
  *
@@ -61,11 +67,11 @@ public class MainViewController implements Initializable {
             }
         });
         datePicker.setValue(LocalDate.now());
-        datePicker.valueProperty().addListener((o,oldV,newV) -> handleCheckAvailability());
-        serviceComboBox.valueProperty().addListener((o,oldV,newV) -> handleCheckAvailability());
-        personnesSpinner.valueProperty().addListener((o,oldV,newV) -> handleCheckAvailability());
-        this.salle = GestionDonnees.chargerPlanSalle() ;
-        this.allReservations = GestionDonnees.chargerReservations() ;
+        datePicker.valueProperty().addListener((o, oldV, newV) -> handleCheckAvailability());
+        serviceComboBox.valueProperty().addListener((o, oldV, newV) -> handleCheckAvailability());
+        personnesSpinner.valueProperty().addListener((o, oldV, newV) -> handleCheckAvailability());
+        this.salle = GestionDonnees.chargerPlanSalle();
+        this.allReservations = GestionDonnees.chargerReservations();
         redrawSalle();
     }
 
@@ -76,13 +82,40 @@ public class MainViewController implements Initializable {
      * @param table l'objet métier Table contenant les dimensions et la forme
      */
     private void centerTextOnTable(Text text, Table table) {
+        if (table.getForme() == Forme.ROND) {
+            text.setTranslateX(-text.getBoundsInLocal().getWidth() / 2);
+            text.setTranslateY(text.getBoundsInLocal().getHeight() / 2);
+        }else{
+            text.setTranslateX(table.getLargeur()/2);
+            text.setTranslateY(table.getHauteur()/2);
+        }
     }
 
     /**
      * Dessine ou redessine l'ensemble des tables dans le Pane graphique.
      */
     private void redrawSalle() {
-        
+        for (Table uneTable : this.salle.getTables()) {
+            Group gTable = new Group();
+            Shape shapeTable = (uneTable.getForme() == Forme.RECTANGLE)
+                    ? new Rectangle(uneTable.getLargeur(), uneTable.getHauteur())
+                    : new Circle(uneTable.getHauteur() / 2);
+            shapeTable.setFill(Paint.valueOf("LEMONCHIFFON"));
+            shapeTable.setStroke(Paint.valueOf("VIOLET"));
+            Text textTable = new Text(uneTable.getCapacite() + " p.");
+            centerTextOnTable(textTable, uneTable);
+            gTable.getChildren().add(shapeTable);
+            gTable.getChildren().add(textTable);
+            gTable.setLayoutX(uneTable.getPositionX());
+            gTable.setLayoutY(uneTable.getPositionY());
+            this.sallePane.getChildren().add(gTable);
+            gTable.setUserData(uneTable);
+            gTable.setOnMouseEntered((event) -> shapeTable.setFill(Paint.valueOf("cyan")) );
+            gTable.setOnMouseExited((event) -> shapeTable.setFill(Paint.valueOf("LEMONCHIFFON")) );
+            gTable.setOnMouseClicked((event) -> handleTableClick(event));
+           
+        }
+        handleCheckAvailability();
     }
 
     /**
@@ -98,6 +131,9 @@ public class MainViewController implements Initializable {
      * réservation.
      */
     private void handleTableClick(MouseEvent event) {
+        Group groupClicked = (Group) event.getSource() ;
+        Table laTable = (Table) groupClicked.getUserData();
+        System.out.println("Clique sur table :" + laTable.getNumero());
     }
 
     /**
